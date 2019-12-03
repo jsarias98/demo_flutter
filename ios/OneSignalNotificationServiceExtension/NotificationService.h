bf865675e005fe309425f4553419121eba7444d5
Omar Plata <omarplata@MacBook-Pro-de-Omar.local>
2019-12-03 16:13:54 -0500

    config onesignal

---
+++ ios/OneSignalNotificationServiceExtension/NotificationService.h
@@ -0,0 +1,13 @@
+//
+//  NotificationService.h
+//  OneSignalNotificationServiceExtension
+//
+//  Created by Omar Plata on 3/12/19.
+//  Copyright © 2019 The Chromium Authors. All rights reserved.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
